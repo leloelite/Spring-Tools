46937ed10ad6bd99fbaccae211709d4783a7a223
joseribeiro@tecgraf.puc-rio.br <joseribeiro@tecgraf.puc-rio.br>
2020-05-10 03:39:31 -0300

    Commit do ultimo rafactor, com os nomes dos pacotes de teste

--- src/test/java/com/profissionais/ti/ProfissionaisTiApplicationTests.java
+++ src/test/java/com/profissionais/ti/ProfissionaisTiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo2;
+package com.profissionais.ti;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Demo2ApplicationTests {
+class ProfissionaisTiApplicationTests {
 
 	@Test
 	void contextLoads() {
